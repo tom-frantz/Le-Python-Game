aca2d46d289fd838e9f2675628098d70f885f390
Tomato150 <tomcat.frantz@gmail.com>
2015-06-07 19:49:32 +1000

    Scouting and Supply_Run

---
+++ zombies.py
@@ -0,0 +1,11 @@
+def loot_zombies():
+    print('Temp holder')
+
+def base_zombies():
+    print('Temp Holder')
+
+def travel_zombies():
+    print('Temp Holder')
+
+def combat_zombies():
+    print('Temp Holder')
