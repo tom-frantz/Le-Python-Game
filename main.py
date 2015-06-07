8376a806390d8b9ee95aa31bb1be946509e5c185
Tomato150 <tomcat.frantz@gmail.com>
2015-06-07 18:42:42 +1000

    Scouting and Supply_Run

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import base_building
 import scouting
 import supply_run
 
-temp2 = True
-while temp2 == True:
-    menu_input = input('What would you like to do. ( Base building, Scouting, Supply run) ').lower()
+while menu_input != 'quit':
+    menu_input = input('What would you like to do. ( Base building, Scouting, Supply run, Quit) ').lower()
     if menu_input == 'b' or menu_input == 'base' or menu_input == 'base building' or menu_input == 'building':
         base_building.main()
     elif menu_input == 'scouting' or menu_input == 'scout':
         scouting.main()
     elif menu_input == 'supply' or menu_input == 'run' or menu_input == 'supply run':
         supply_run.main()
-    else:
-        print('Plox redo bb')
+print('Out of menu.')
---
+++ sysvars.py
@@ -0,0 +1,4 @@
+class SystemVariables(object):
+    def __int__(self):
+        self.fruit = ['Apples', 'Pears', 'Plums']
+        self.vegetables = ['Corn', 'Potato', 'Beans']
