0fc86a4e7181183719956b6b122659073994b443
Tomato150 <tomcat.frantz@gmail.com>
2015-06-07 17:31:18 +1000

    Scouting and Supply_Run

---
+++ scouting.py
@@ -0,0 +1,7 @@
+def main():
+    print('''What region would you like to explore.
+( Town 1, Town 2, Town 3, Town 4, Town 5 )''')
+    region = input().lower()
+    print(region)
+    print('You arrive at ' +  region +  ' safely and begin to scan the area, you see three buildings. What building do you want to search ( Building 1, Building 2, Building 3 )')
+    building = input().lower()
--- supply_run.py
+++ supply_run.py
@@ -2,12 +2,13 @@ def main():
 
     import random
 
-    region = input().lower()
+
     print(''' what region would you like to explore. (Town1,Town2,Town3,Town4,Town5 )''')
+    region = input().lower()
     print('You arrive at ' + str(region) + ' safely and begin to scout the area, you see '"(building1,building2,building3")
     Building = input()
 
-    loot = random.choice = ['Food', 'Water', 'Medical supplies', 'Building supplies', 'scrap', 'nothing']
+    loot = random.choice(['Food', 'Water', 'Medical supplies', 'Building supplies', 'scrap', 'nothing'])
     if loot == 'Food':
         print('you found some food')
     elif loot == 'Water':
