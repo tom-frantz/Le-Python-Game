ba8b79efe4a7de43c67e37a1e7fe8b6601c18639
Tomato150 <tomcat.frantz@gmail.com>
2015-06-07 17:43:44 +1000

    Scouting and Supply_Run

--- scouting.py
+++ scouting.py
@@ -1,7 +1,9 @@
+import random
 def main():
-    print('''What region would you like to explore.
+    print('''What region would you like to explore:
 ( Town 1, Town 2, Town 3, Town 4, Town 5 )''')
     region = input().lower()
-    print(region)
-    print('You arrive at ' +  region +  ' safely and begin to scan the area, you see three buildings. What building do you want to search ( Building 1, Building 2, Building 3 )')
+    print('''You arrive at ''' + region + ''' safely and begin to scan the area, you see three buildings. What building do you want to search:
+( Building 1, Building 2, Building 3 )''')
     building = input().lower()
+
--- supply_run.py
+++ supply_run.py
@@ -1,11 +1,12 @@
 import random
 
 def main():
-    print(''' what region would you like to explore. (Town1,Town2,Town3,Town4,Town5 )''')
+    print(''' what region would you like to explore:
+( Town1, Town2, Town3, Town4, Town5 )''')
     region = input().lower()
-    print('You arrive at ' + str(region) + ' safely and begin to scout the area, you see '"(building1,building2,building3")
+    print('''You arrive at ''' + str(region) + ''' safely and begin to scout the area, you see:
+( building1, building2, building3 )''')
     Building = input()
-
     loot = random.choice(['Food', 'Water', 'Medical supplies', 'Building supplies', 'scrap', 'nothing'])
     print('You have found', loot)
     zombies = random.choice(['yes', 'no'])
@@ -13,6 +14,5 @@ def main():
             print('there are zombies around here, might be some good loot')
     print('Do you want to go in')
     loot_building = input()
-
 
 main()
