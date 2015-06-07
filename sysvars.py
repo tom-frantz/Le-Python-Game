9b83fd58e718330b0ee9872f09101914a0c4d8aa
Tomato150 <tomcat.frantz@gmail.com>
2015-06-07 19:03:12 +1000

    Scouting and Supply_Run

--- sysvars.py
+++ sysvars.py
@@ -1,4 +1,4 @@
 class SystemVariables(object):
     def __init__(self):
         self.fruit = ['Apples', 'Pears', 'Plums']
-        self.vegetables = ['Corn', 'Potato', 'Beans']
+        self.vegetables = ['Beans', 'Broccoli' 'Corn', 'Potato',]
